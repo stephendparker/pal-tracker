efa53f489f5b2b36276448149c2c910ac0aaeea5
Brad <t.bradley.owens@gmail.com>
2018-05-07 12:41:00 -0600

    environment stuff

---
+++ src/main/java/io/pivotal/pal/tracker/EnvController.java
@@ -0,0 +1,41 @@
+package io.pivotal.pal.tracker;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class EnvController {
+
+    String port;
+    String memoryLimit;
+    String cfInstanceIndex;
+    String cfInstanceAddress;
+
+    Map<String, String> envMap = new HashMap<String, String>();
+
+    public EnvController(@Value("${PORT:NOT SET}") String port,
+                         @Value("${MEMORY_LIMIT:NOT SET}") String memoryLimit,
+                         @Value("${CF_INSTANCE_INDEX:NOT SET}") String cfInstanceIndex,
+                         @Value("${CF_INSTANCE_ADDR:NOT SET}") String cfInstanceAddress) {
+
+        this.port = port;
+        this.memoryLimit = memoryLimit;
+        this.cfInstanceIndex = cfInstanceIndex;
+        this.cfInstanceAddress = cfInstanceAddress;
+
+        envMap.put("PORT", port);
+        envMap.put("MEMORY_LIMIT", memoryLimit);
+        envMap.put("CF_INSTANCE_INDEX", cfInstanceIndex);
+        envMap.put("CF_INSTANCE_ADDR", cfInstanceAddress);
+    }
+
+    @GetMapping("/env")
+    public Map<String, String> getEnv() {
+
+        return envMap;
+    }
+}
